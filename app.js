e5c0bfa6b078b2ecfdd7f0db607c1647396cc0a5
killowatt <killowatt2213@gmail.com>
2021-11-01 18:30:31 -0400

    Fix formatting in app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var url=require('url');
+var url = require('url');
 
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
     html = fs.readFileSync('index.html');
 
-var log = function(entry) {
+var log = function (entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
 
@@ -13,24 +13,24 @@ var server = http.createServer(function (req, res) {
     if (req.method === 'POST') {
         var body = '';
 
-        req.on('data', function(chunk) {
+        req.on('data', function (chunk) {
             body += chunk;
         });
 
-        req.on('end', function() {
+        req.on('end', function () {
             if (req.url === '/') {
                 log('Received message: ' + body);
             } else if (req.url = '/scheduled') {
                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
             }
 
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
             res.end();
         });
     } else {
-        var pathname=url.parse(req.url).pathname;
+        var pathname = url.parse(req.url).pathname;
         res.writeHead(200);
-        switch(pathname){
+        switch (pathname) {
             case '/quiz.html':
                 html = fs.readFileSync('quiz.html');
                 break;
