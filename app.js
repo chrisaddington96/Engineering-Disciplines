59d0c6db51734428a56ecbef9a89691c9b61d36e
chrisaddington96 <47895366+chrisaddington96@users.noreply.github.com>
2021-11-01 16:42:58 -0400

    Reworked
    Reworked for node js

---
+++ app.js
@@ -0,0 +1,53 @@
+var url=require('url');
+
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
+
+var log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received message: ' + body);
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        var pathname=url.parse(req.url).pathname;
+        res.writeHead(200);
+        switch(pathname){
+            case '/quiz.html':
+                html = fs.readFileSync('quiz.html');
+                break;
+            case '/submit.html':
+                html = fs.readFileSync('submit.html');
+                break;
+            default:
+                html = fs.readFileSync('index.html');
+                break;
+        }
+        res.write(html);
+        res.end();
+    }
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
